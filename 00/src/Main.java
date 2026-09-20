d3a089e46f9bf701e1ec8b15b8c458027d2750ab
davimissola <davigarciamissola@gmail.com>
2026-09-19 23:20:59 -0300

    a

--- 00/src/Main.java
+++ 00/src/Main.java
@@ -1,3 +1,4 @@
+import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Movie;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -7,7 +8,14 @@ public class Main {
         myMovie.setName("Seven");
         myMovie.setAnoLancamento(2004);
         myMovie.setIncluidoNoPlano(true);
+        myMovie.setDuracaoEmMinutos(150);
         myMovie.showMovie();
+
+        Movie myMovie2 = new Movie();
+        myMovie2.setDuracaoEmMinutos(190);
+
+        Movie myMovie3 = new Movie();
+        myMovie3.setDuracaoEmMinutos(130);
 
 
         Serie mentalista = new Serie();
@@ -16,5 +24,12 @@ public class Main {
         mentalista.setEpisodios(20);
         mentalista.setMinutosPorEpisodio(50);
         System.out.println(mentalista.getDuracaoEmMinutos() + " minutos");
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.incluir(myMovie);
+        calculadora.incluir(myMovie2);
+        calculadora.incluir(myMovie3);
+        calculadora.incluir(mentalista);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
